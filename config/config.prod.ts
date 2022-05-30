73f7b09934b142eedfbb25560ac2532694f9bded
m2 <467057463@qq.com>
2022-05-30 21:10:00 +0800

    change config

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,6 +1,17 @@
 import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
-  const config: PowerPartial<EggAppConfig> = {};
+  const config: PowerPartial<EggAppConfig> = {
+    mongoose: {
+      client: {
+        url: 'mongodb://localhost/blog',
+        options: {
+          useNewUrlParser: true,
+          user: 'xxx',
+          pass: 'xxxxxx'
+        },
+      }
+    },
+  };
   return config;
 };
