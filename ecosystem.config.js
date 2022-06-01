4798aa82aea9b410778f7bd7c9248f3112c56846
毛有文 <467057463@qq.com>
2022-06-01 18:26:41 +0800

    change

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps : [
+    {
+      name: 'blog_back_egg',
+      script: 'npm',
+      args: 'run start',
+      script: 'index.js',
+      env: {
+        "COMMON_VARIABLE": "true"
+      },
+      env_production: {
+        "NODE_ENV": "production"
+      }
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '95.179.164.10',
+      ref  : 'origin/main',
+      repo : 'git@github.com:467057463/blog_back_egg.git',
+      path : '/var/www/blog_back_egg',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && npm run tsc && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  }
+};
